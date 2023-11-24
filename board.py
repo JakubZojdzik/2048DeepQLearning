a16aeea0f52e52028fbf795be2dcd1f2ac00ef37
JakubZojdzik <jakub.zojdzik@gmail.com>
2023-11-24 14:40:14 +0100

    animate tiles

--- board.py
+++ board.py
@@ -4,19 +4,22 @@ import random
 class Board:
     def __init__(self, pos_x, pos_y):
         self.grid = [[None for _ in range(4)] for _ in range(4)]
+        self.trash = []
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.texture = pygame.image.load('assets/board.png').convert()
-        self.grid[0][2] = Tile(2)
-        self.grid[1][1] = Tile(4)
-        self.grid[3][3] = Tile(2)
+        # self.grid[0][2] = Tile(2, 0, 2, self.pos_x, self.pos_y)
+        # self.grid[1][1] = Tile(4, 1, 1, self.pos_x, self.pos_y)
+        # self.grid[3][3] = Tile(2, 3, 3, self.pos_x, self.pos_y)
+        self.grid[0][3] = Tile(2, 0, 3, self.pos_x, self.pos_y)
+        self.grid[0][2] = Tile(2, 0, 2, self.pos_x, self.pos_y)
 
     def add_tile(self):
         empty_positions = [(i, j) for i in range(4) for j in range(4) if self.grid[i][j] is None]
         if empty_positions:
             i, j = random.choice(empty_positions)
             value = random.choices([2, 4], weights=[0.9, 0.1], k=1)[0]
-            self.grid[i][j] = Tile(value)
+            self.grid[i][j] = Tile(value, i, j, self.pos_x, self.pos_y, 0)
 
     def move_tiles(self, direction):
         for i in range(4):
@@ -31,12 +34,15 @@ class Board:
                         if(self.grid[x][y] is None):
                             continue
                         if(self.grid[x][y-1] is None):
+                            self.grid[x][y].move(x, y-1)
                             self.grid[x][y-1] = self.grid[x][y]
                             self.grid[x][y] = None
                             action = True
                         elif(self.grid[x][y-1].value == self.grid[x][y].value and self.grid[x][y-1].joinable and self.grid[x][y].joinable):
                             self.grid[x][y-1].update(self.grid[x][y-1].value * 2)
                             self.grid[x][y-1].joinable = False
+                            self.grid[x][y].move(x, y-1)
+                            self.trash.append(self.grid[x][y])
                             self.grid[x][y] = None
                             action = True
         elif(direction == 'd'):
@@ -46,13 +52,16 @@ class Board:
                         if(self.grid[x][y] is None):
                             continue
                         if(self.grid[x][y+1] is None):
+                            self.grid[x][y].move(x, y+1)
                             self.grid[x][y+1] = self.grid[x][y]
                             self.grid[x][y] = None
                             action = True
                         elif(self.grid[x][y+1].value == self.grid[x][y].value and self.grid[x][y+1].joinable and self.grid[x][y].joinable):
                             self.grid[x][y+1].update(self.grid[x][y+1].value * 2)
-                            self.grid[x][y] = None
                             self.grid[x][y+1].joinable = False
+                            self.grid[x][y].move(x, y+1)
+                            self.trash.append(self.grid[x][y])
+                            self.grid[x][y] = None
                             action = True
         elif(direction == 'l'):
             for itr in range(3):
@@ -61,13 +70,16 @@ class Board:
                         if(self.grid[x][y] is None):
                             continue
                         if(self.grid[x-1][y] is None):
+                            self.grid[x][y].move(x-1, y)
                             self.grid[x-1][y] = self.grid[x][y]
                             self.grid[x][y] = None
                             action = True
                         elif(self.grid[x-1][y].value == self.grid[x][y].value and self.grid[x-1][y].joinable and self.grid[x][y].joinable):
                             self.grid[x-1][y].update(self.grid[x-1][y].value * 2)
-                            self.grid[x][y] = None
                             self.grid[x-1][y].joinable = False
+                            self.grid[x][y].move(x-1, y)
+                            self.trash.append(self.grid[x][y])
+                            self.grid[x][y] = None
                             action = True
         elif(direction == 'r'):
             for itr in range(3):
@@ -76,26 +88,29 @@ class Board:
                         if(self.grid[x][y] is None):
                             continue
                         if(self.grid[x+1][y] is None):
+                            self.grid[x][y].move(x+1, y)
                             self.grid[x+1][y] = self.grid[x][y]
                             self.grid[x][y] = None
                             action = True
                         elif(self.grid[x+1][y].value == self.grid[x][y].value and self.grid[x+1][y].joinable and self.grid[x][y].joinable):
                             self.grid[x+1][y].update(self.grid[x+1][y].value * 2)
-                            self.grid[x][y] = None
                             self.grid[x+1][y].joinable = False
+                            self.trash.append(self.grid[x][y])
+                            self.grid[x][y].move(x+1, y)
+                            self.grid[x][y] = None
                             action = True
         if action:
             self.add_tile()
 
-        # for y in range(4):
-        #     for x in range(4):
-        #         if(self.grid[x][y] is None):
-        #             print("0", end=" ")
-        #         else:
-        #             print(self.grid[x][y].value, end=" ")
-        #     print()
-        # print()
-        # print()
+        for y in range(4):
+            for x in range(4):
+                if(self.grid[x][y] is None):
+                    print("0", end=" ")
+                else:
+                    print(self.grid[x][y].value, end=" ")
+            print()
+        print()
+        print()
 
     def is_game_over(self):
         # Check if the game is over (e.g., no more valid moves)
@@ -106,7 +121,15 @@ class Board:
 
     def draw(self, screen):
         screen.blit(self.texture, (self.pos_x, self.pos_y))
+
+        for t_tile in self.trash:
+            t_tile.draw(screen)
+            t_tile.animate()
+            if(t_tile.pos_x == t_tile.target_x and t_tile.pos_y == t_tile.target_y):
+                self.trash.remove(t_tile)
+
         for x in range(4):
             for y in range(4):
                 if self.grid[x][y] is not None:
-                    self.grid[x][y].draw(screen, x, y, self.pos_x, self.pos_y)
+                    self.grid[x][y].draw(screen)
+                    self.grid[x][y].animate()
--- tile.py
+++ tile.py
@@ -3,18 +3,39 @@ import pygame
 texture_path = 'assets/tiles/'
 tile_size = 85
 padding = 12
+move_speed = 4
+alpha_speed = 1
 
 class Tile:
-    def __init__(self, value):
+    def __init__(self, value, col, row, offset_x, offset_y, alpha=255):
         self.value = value
         self.texture = pygame.image.load(texture_path + str(value) + '.png').convert()
         self.joinable = True
+        self.pos_x = padding * (col + 1) + col * tile_size + offset_x
+        self.pos_y = padding * (row + 1) + row * tile_size + offset_y
+        self.target_x = self.pos_x
+        self.target_y = self.pos_y
+        self.offset_x = offset_x
+        self.offset_y = offset_y
+        self.alpha = alpha
+        self.texture.set_alpha(self.alpha)
 
     def update(self, value):
         self.value = value
         self.texture = pygame.image.load(texture_path + str(value) + '.png').convert()
 
-    def draw(self, screen, col, row, offset_x, offset_y):
-        x = padding * (col + 1) + col * tile_size + offset_x
-        y = padding * (row + 1) + row * tile_size + offset_y
-        screen.blit(self.texture, (x, y))
+    def animate(self):
+        if(self.pos_x != self.target_x):
+            self.pos_x += min(move_speed * ((self.target_x - self.pos_x) / abs(self.target_x - self.pos_x)), self.target_x - self.pos_x, key=abs)
+        if(self.pos_y != self.target_y):
+            self.pos_y += min(move_speed * ((self.target_y - self.pos_y) / abs(self.target_y - self.pos_y)), self.target_y - self.pos_y, key=abs)
+        if(self.alpha < 255):
+            self.alpha += min(alpha_speed, 255 - self.alpha)
+            self.texture.set_alpha(self.alpha)
+
+    def move(self, col, row):
+        self.target_x = padding * (col + 1) + col * tile_size + self.offset_x
+        self.target_y = padding * (row + 1) + row * tile_size + self.offset_y
+
+    def draw(self, screen):
+        screen.blit(self.texture, (self.pos_x, self.pos_y))
